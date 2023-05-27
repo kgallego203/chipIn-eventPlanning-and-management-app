15b34df3ceeb65cc15b579bb78035ee1f3ae1f02
Kyle Brent Gallego <kylebrentgallegocpe@gmail.com>
2023-05-27 21:54:34 +0800

    initial commit in this branch

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,15 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:appwrite/appwrite.dart';
 
 class AppwriteAuth {
+  // Method to sign up a new user
   static void signUp(String firstname, String lastname, String username,
       String email, String password) {
+    // Create a new Appwrite client and account instance
     Client client = Client();
     Account account = Account(client);
 
+    // Set the Appwrite endpoint and project ID
     client
             .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
             .setProject('6471c510263d6b4e5e77') // Your project ID
         ;
+    // Call the Appwrite account create method to sign up the user
     Future result = account.create(
       name: '$firstname $lastname',
       userId: username,
@@ -19,6 +23,7 @@ class AppwriteAuth {
       password: password,
     );
 
+    // Handle the result of the sign up request
     result.then((response) {
       print(response);
     }).catchError((error) {
@@ -26,23 +31,29 @@ class AppwriteAuth {
     });
   }
 
+  // Method to log in an existing user
   static void login(BuildContext context, String email, String password) {
+    // Create a new Appwrite client and account instance
     Client client = Client();
     Account account = Account(client);
 
+    // Set the Appwrite endpoint and project ID
     client
             .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
             .setProject('6471c510263d6b4e5e77') // Your project ID
         ;
+    // Call the Appwrite account createEmailSession method to log in the user
     Future result = account.createEmailSession(
       email: email,
       password: password,
     );
 
+    // Handle the result of the login request
     result.then(
       (response) {
         print(response);
 
+        // Show a snackbar with a success message and navigate to the home screen
         const snackBar = SnackBar(
           content: Text('Login Success'),
         );
@@ -59,6 +70,7 @@ class AppwriteAuth {
       (error) {
         print(error.response);
 
+        // Show a snackbar with an error message
         const snackBar = SnackBar(
           content: Text('Login Failed'),
         );
