54088c4629ad8a394f89032c0fc826ef19836551
Kyle Brent Gallego <kylebrentgallegocpe@gmail.com>
2023-05-30 20:01:09 +0800

    create 3 appwrite constants

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,7 @@
+// Values that won't be changed throughout the execution of the program
+
+class AppwriteConstants {
+  static const String databaseId = '64734e007f78d838e9d4';
+  static const String projectId = '6471c510263d6b4e5e77';
+  static const String endPoint = 'https://appwrite.io/v1';
+}
