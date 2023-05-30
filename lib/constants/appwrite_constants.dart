c8da7c07ce985a54c3c1252e4cf7ffca592b6596
Kyle Brent Gallego <kylebrentgallegocpe@gmail.com>
2023-05-30 23:42:47 +0800

    add collection constants

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -2,4 +2,9 @@ class AppwriteConstants {
   static const String databaseId = '64734e007f78d838e9d4';
   static const String projectId = '6471c510263d6b4e5e77';
   static const String endPoint = 'https://cloud.appwrite.io/v1';
+
+  static const String eventsCollection = '6475dd94e90d6c47cf0b';
+  static const String usersCollection = '6475d82d54547e0be8c9';
+  static const String attendeesCollection = '6475de40d97bb756a8fd';
+  static const String notificationsCollection = '6475dedab82ae245b3eb';
 }
