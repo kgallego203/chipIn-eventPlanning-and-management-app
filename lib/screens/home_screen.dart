f862f3a45741917091409cc86b518acfbee796a0
Kyle Brent Gallego <kylebrentgallegocpe@gmail.com>
2023-05-30 15:28:10 +0800

    add a button in the home screen to test the event creation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,44 @@
+import 'package:chip_in/config/appwrite_config.dart';
+import 'package:chip_in/services/authentication_service.dart';
+import 'package:chip_in/services/event_service.dart';
 import 'package:flutter/material.dart';
+import 'event_creation_screen.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => EventCreationScreen(
+                    eventService: EventService(
+                        client: AppwriteAuth.client,
+                        endpoint: AppwriteConfig.endPoint,
+                        projectId: AppwriteConfig.projectId),
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text('Welcome to the Home Page!'),
+      ),
+    );
   }
 }
