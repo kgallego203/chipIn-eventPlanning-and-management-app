9ecb7a5913a88d9c923e8c58e2fce2abaac5bed8
Kyle Brent Gallego <kylebrentgallegocpe@gmail.com>
2023-05-31 14:38:21 +0800

    make a title in the auth screen

--- lib/features/auth/view/authentication_screen.dart
+++ lib/features/auth/view/authentication_screen.dart
@@ -10,8 +10,32 @@ class AuthenticationScreen extends StatelessWidget {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
+          title: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'chipIn',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Pallete
+                      .primary100, // Set the color of the "chipIn" text to primary100 from the Pallete class
+                ),
+              ),
+              Text(
+                'powered by Flutter and Appwrite',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Pallete
+                      .neutral70, // Set the color of the "powered by Flutter and Appwrite" text to neutral60 from the Pallete class
+                ),
+              ),
+            ],
+          ),
+          backgroundColor: Pallete
+              .neutral0, // Set the background color of the app bar to neutral0 from the Pallete class
           bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(48),
+            preferredSize: const Size.fromHeight(72),
             child: Container(
               color: Pallete
                   .neutral0, // Set the background color of the tab bar to neutral0 from the Pallete class
@@ -34,13 +58,10 @@ class AuthenticationScreen extends StatelessWidget {
                 labelColor: Pallete
                     .neutral0, // Set the color of the selected tab label to neutral0 from the Pallete class
                 unselectedLabelColor: Pallete
-                    .neutral70, // Set the color of the unselected tab label to neutral40 from the Pallete class
+                    .neutral70, // Set the color of the unselected tab label to neutral70 from the Pallete class
               ),
             ),
           ),
-          title: const Text('chipIn'),
-          backgroundColor: Pallete
-              .neutral0, // Set the background color of the app bar to neutral0 from the Pallete class
         ),
         body: TabBarView(
           children: [
