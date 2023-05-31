97f3bf21a3389656c69dba076f5e574842d7b743
Kyle Brent Gallego <kylebrentgallegocpe@gmail.com>
2023-05-31 15:17:44 +0800

    add more comments

--- lib/themes/theme.dart
+++ lib/themes/theme.dart
@@ -1,2 +1,6 @@
+// This code exports the contents of the 'palette.dart' and 'app_theme.dart' files
+// so that they can be used in other files that import 'theme.dart'.
+// This allows us to keep our code organized and modular, and makes it easier to
+// reuse code across different parts of our application.
 export './palette.dart';
 export 'app_theme.dart';
