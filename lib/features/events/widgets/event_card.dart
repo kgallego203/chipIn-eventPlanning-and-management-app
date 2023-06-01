e5a182f71f0b672a296d48621ae4932e57f66409
Kyle Brent Gallego <kylebrentgallegocpe@gmail.com>
2023-06-01 22:07:42 +0800

    add ui design for event cards and edit the date to have a specific desirable format

--- lib/features/events/widgets/event_card.dart
+++ lib/features/events/widgets/event_card.dart
@@ -1,4 +1,6 @@
+import 'package:chip_in/themes/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:chip_in/features/events/models/event_model.dart';
 
 class EventCard extends StatelessWidget {
@@ -8,39 +10,97 @@ class EventCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dateFormat = DateFormat('dd MMMM yyyy');
+    final timeFormat = DateFormat('hh:mm a');
+
     return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      elevation: 4.0,
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
               event.title,
-              style: const TextStyle(
-                fontSize: 24,
+              style: TextStyle(
+                fontSize: 20.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 8.0),
+            SizedBox(height: 8.0),
             Text(
-              event.dateTime.toString(), // You might want to format this date
-              style: const TextStyle(
-                fontSize: 18,
+              dateFormat.format(event.dateTime),
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Pallete.neutral70,
               ),
             ),
-            const SizedBox(height: 8.0),
+            SizedBox(height: 8.0),
+            Text(
+              timeFormat.format(event.dateTime),
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Pallete.neutral70,
+              ),
+            ),
+            SizedBox(height: 8.0),
             Text(
               event.location,
-              style: const TextStyle(
-                fontSize: 18,
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Pallete.neutral70,
               ),
             ),
-            const SizedBox(height: 8.0),
-            Text(
-              event.description,
-              style: const TextStyle(
-                fontSize: 18,
-              ),
+            SizedBox(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Join',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: Pallete.neutral0,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Pallete.success100,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 24.0,
+                      vertical: 12.0,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Details',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: Pallete.neutral0,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Pallete.primary100,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 24.0,
+                      vertical: 12.0,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
