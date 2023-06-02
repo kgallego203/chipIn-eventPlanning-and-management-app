5a2887babf0e94918ca485e46d8074a2f8b56fe2
Kyle Gallego <kylebrentgallegocpe@gmail.com>
2023-06-02 23:36:22 +0800

    Add comments to the event file

--- lib/features/events/services/event_service.dart
+++ lib/features/events/services/event_service.dart
@@ -11,15 +11,17 @@ class EventService {
       {required this.client, required this.endpoint, required this.projectId});
 
   Future<bool> createEvent(Event event) async {
-    // Initialize the client
+    // Initialize the client with the endpoint and project ID
     client
             .setEndpoint(AppwriteConstants.endPoint) // Your API Endpoint
             .setProject(AppwriteConstants.projectId) // Your project ID
         ;
 
+    // Create a Databases object using the initialized client
     Databases databases = Databases(client);
 
     try {
+      // Create a new document in the events collection with the provided event data
       await databases.createDocument(
         databaseId: AppwriteConstants.databaseId,
         collectionId: AppwriteConstants.eventsCollection,
@@ -28,6 +30,7 @@ class EventService {
       );
       return true;
     } catch (e) {
+      // If there was an error, log the error and return false
       print('Failed to create event: $e');
       return false;
     }
@@ -37,20 +40,24 @@ class EventService {
   Future<List<Event>> getAllEvents() async {
     List<Event> eventList = [];
 
+    // Create a Databases object using the initialized client
     Databases databases = Databases(client);
 
     try {
+      // Get all documents in the events collection
       var response = await databases.listDocuments(
         databaseId: AppwriteConstants.databaseId,
         collectionId: AppwriteConstants.eventsCollection,
       );
 
+      // If there are documents, add them to the eventList
       if (response.documents.isNotEmpty) {
         for (var item in response.documents) {
           eventList.add(Event.fromJson(item.data));
         }
       }
     } catch (e) {
+      // If there was an error, log the error
       print('Failed to get events: $e');
     }
     return eventList;
