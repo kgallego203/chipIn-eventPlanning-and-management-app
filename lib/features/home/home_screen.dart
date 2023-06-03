76495a2a05d5ba42d8e188b66cb1ef1ca004039a
Kyle Brent Gallego <kylebrentgallegocpe@gmail.com>
2023-06-03 22:34:37 +0800

    redo the home_screen for simplicity (for now)

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,181 +1,125 @@
 import 'package:chip_in/features/auth/services/auth_service.dart';
-import 'package:chip_in/features/events/models/event_model.dart';
 import 'package:chip_in/features/events/services/event_service.dart';
 import 'package:chip_in/features/events/view/event_creation_screen.dart';
 import 'package:chip_in/features/events/view/event_joining_screen.dart';
-import 'package:chip_in/features/events/widgets/event_card.dart';
 import 'package:chip_in/themes/palette.dart';
 import 'package:flutter/material.dart';
 import '/constants/appwrite_constants.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  int _selectedIndex = 0;
-  late AnimationController _animationController;
-  late Animation<double>? _animation;
-  late EventService eventService;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 300),
-    );
-    _animation = Tween<double>(begin: 0, end: 1).animate(_animationController);
-
-    eventService = EventService(
+  Widget build(BuildContext context) {
+    final eventService = EventService(
       client: AppwriteAuth.client, // ensure you have AppwriteAuth.client
       endpoint: AppwriteConstants.endPoint,
       projectId: AppwriteConstants.projectId,
     );
-  }
 
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  void _toggleOptions() {
-    if (_animationController.isCompleted) {
-      _animationController.reverse();
-    } else {
-      _animationController.forward();
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Pallete.neutral0,
-      appBar: AppBar(
-        title: const Text('Home Page'),
-        backgroundColor: Pallete.primary200,
-        automaticallyImplyLeading: false,
-      ),
-      body: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  'Event Listing',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
+    return Theme(
+      data: ThemeData(
+        primaryColor: Pallete.primary200,
+        backgroundColor: Pallete.neutral0,
+        scaffoldBackgroundColor: Pallete.neutral0,
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            color: Pallete.neutral100,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+          bodyText1: TextStyle(
+            color: Pallete.neutral100,
+            fontSize: 16,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Pallete.primary200),
+            textStyle: MaterialStateProperty.all(
+              const TextStyle(
+                color: Pallete.neutral0,
+                fontSize: 16,
               ),
-              Expanded(
-                child: FutureBuilder<List<Event>>(
-                  future: eventService.getAllEvents(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return ListView.builder(
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (context, index) {
-                          return EventCard(event: snapshot.data![index]);
-                        },
-                      );
-                    } else if (snapshot.hasError) {
-                      return Text('Failed to load events: ${snapshot.error}');
-                    } else {
-                      return const CircularProgressIndicator();
-                    }
-                  },
-                ),
+            ),
+          ),
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text('Home Page'),
+          backgroundColor: Theme.of(context).primaryColor,
+          actions: [
+            PopupMenuButton(
+              itemBuilder: (BuildContext context) {
+                return const [
+                  PopupMenuItem(
+                    value: 'view_profile',
+                    child: Text('View Profile'),
+                  ),
+                  PopupMenuItem(
+                    value: 'logout',
+                    child: Text('Logout'),
+                  ),
+                ];
+              },
+              onSelected: (value) {
+                if (value == 'view_profile') {
+                  // TODO: Implement view profile functionality
+                } else if (value == 'logout') {
+                  // TODO: Implement logout functionality
+                }
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => EventCreationScreen(
+                        eventService: eventService,
+                      ),
+                    ),
+                  );
+                },
+                child: const Text('Create Event'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  // TODO: Implement My Created Events functionality
+                },
+                child: const Text('My Created Events'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const EventJoiningScreen(),
+                    ),
+                  );
+                },
+                child: const Text('Join Events'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  // TODO: Implement My Joined Events functionality
+                },
+                child: const Text('My Joined Events'),
               ),
             ],
           ),
-          AnimatedBuilder(
-            animation: _animationController,
-            builder: (context, child) {
-              return Positioned(
-                bottom: 80 * (_animation?.value ?? 0),
-                right: 16,
-                child: Opacity(
-                  opacity: _animation?.value ?? 0,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      FloatingActionButton.extended(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => EventCreationScreen(
-                                eventService: eventService,
-                              ),
-                            ),
-                          );
-                          _toggleOptions();
-                        },
-                        label: const Text('Create Event'),
-                        icon: const Icon(Icons.add),
-                        backgroundColor: Pallete.success100,
-                      ),
-                      const SizedBox(height: 16),
-                      FloatingActionButton.extended(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const EventJoiningScreen(),
-                            ),
-                          );
-                          _toggleOptions();
-                        },
-                        label: const Text('Join Event'),
-                        icon: const Icon(Icons.group_add),
-                        backgroundColor: Pallete.information100,
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _toggleOptions,
-        child: const Icon(Icons.add),
-        backgroundColor: Pallete.primary200,
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.event),
-            label: 'My Events',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.event_available),
-            label: 'My Own Events',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.logout),
-            label: 'Logout',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Pallete.primary300,
-        unselectedItemColor: Pallete.neutral50,
-        onTap: _onItemTapped,
-        backgroundColor: Pallete.neutral10,
+        ),
       ),
     );
   }
