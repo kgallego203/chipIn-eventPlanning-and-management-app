1e0970eadfae24c9122ffbd9d8e9c92b11d739a0
Kyle Gallego <kylebrentgallegocpe@gmail.com>
2023-06-03 23:32:36 +0800

    organize and annotate the event creation screen file

--- lib/features/events/view/event_creation_screen.dart
+++ lib/features/events/view/event_creation_screen.dart
@@ -6,18 +6,15 @@ import 'package:chip_in/features/events/models/event_model.dart';
 
 class EventCreationScreen extends StatefulWidget {
   final EventService eventService;
-
-  EventCreationScreen({required this.eventService});
+  const EventCreationScreen({required this.eventService});
 
   @override
   _EventCreationScreenState createState() => _EventCreationScreenState();
 }
 
 class _EventCreationScreenState extends State<EventCreationScreen> {
-  // Key for accessing the form state
-  final _formKey = GlobalKey<FormState>();
-
-  // Form fields
+  final _formKey = GlobalKey<FormState>(); // Instantiating a GlobalKey
+  // Instance Variables
   late String _id;
   late String _title;
   DateTime _date = DateTime.now();
@@ -25,7 +22,9 @@ class _EventCreationScreenState extends State<EventCreationScreen> {
   late String _location;
   late String _description;
 
-  // Function to show a date picker and update the selected date
+/* ------------------------------------------------------------------------------------------------------------------------------ */
+  /// * This function is named _selectDate() and it is a Future that shows a date picker dialog to the user and updates the _date
+  /// * instance variable with the selected date. It is called when the user taps on the "Select Date" button in the EventCreationScreen widget.
   Future<void> _selectDate() async {
     final DateTime? picked = await showDatePicker(
       context: context,
@@ -40,7 +39,9 @@ class _EventCreationScreenState extends State<EventCreationScreen> {
     }
   }
 
-  // Function to show a time picker and update the selected time
+/* ------------------------------------------------------------------------------------------------------------------------------ */
+  /// * This code defines a function named _selectTime() that shows a time picker dialog to the user and updates the _time instance
+  /// * variable with the selected time. It is called when the user taps on the "Select Time" button in the EventCreationScreen widget.
   Future<void> _selectTime() async {
     final TimeOfDay? newTime = await showTimePicker(
       context: context,
@@ -53,18 +54,19 @@ class _EventCreationScreenState extends State<EventCreationScreen> {
     }
   }
 
-  // Function called when the form is submitted
+/* ------------------------------------------------------------------------------------------------------------------------------ */
+  /// * This function called _submit() is responsible for validating the form and creating a new event based on the user's input.
+  /// * It also saves the new event to the database using the eventService instance passed to the EventCreationScreen widget.
+  /// * If the event is successfully created, it shows a success message and resets the form. If it fails, it shows an error message.
   void _submit() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
 
-      // Combine date and time into a single DateTime object
       DateTime dateTime = DateTime(
           _date.year, _date.month, _date.day, _time.hour, _time.minute);
 
-      // Create a new Event object with the entered data
       Event newEvent = Event(
-        id: _id, // provide a value for the id parameter
+        id: _id,
         title: _title,
         dateTime: dateTime,
         location: _location,
@@ -72,15 +74,12 @@ class _EventCreationScreenState extends State<EventCreationScreen> {
         creatorId: await AppwriteAuth.getCreatorId(),
       );
 
-      // Call the event service to create the event
       bool success = await widget.eventService.createEvent(newEvent);
       if (success) {
-        // Show a success message if the event creation was successful
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Event created successfully!')),
         );
 
-        // Clear form fields and navigate back to home screen
         setState(() {
           _title = '';
           _date = DateTime.now();
@@ -90,7 +89,6 @@ class _EventCreationScreenState extends State<EventCreationScreen> {
         });
         Navigator.pop(context);
       } else {
-        // Show an error message if the event creation failed
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Failed to create event.')),
         );
@@ -98,16 +96,20 @@ class _EventCreationScreenState extends State<EventCreationScreen> {
     }
   }
 
+/* ------------------------------------------------------------------------------------------------------------------------------ */
+  /// * UI SECTION
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Create Event')),
+      appBar: AppBar(
+        title: const Text('Create Event'),
+        leading: Container(), // Remove the back button from the app bar
+      ),
       body: Form(
         key: _formKey,
         child: ListView(
           padding: const EdgeInsets.all(16.0),
           children: [
-            // Text input field for the event title
             TextFormField(
               decoration: const InputDecoration(
                 labelText: 'Title',
@@ -123,7 +125,6 @@ class _EventCreationScreenState extends State<EventCreationScreen> {
                   value!.isEmpty ? 'Please enter a title' : null,
               onSaved: (value) => _title = value!,
             ),
-            // Text input field for the event location
             TextFormField(
               decoration: const InputDecoration(
                 labelText: 'Location',
@@ -139,7 +140,6 @@ class _EventCreationScreenState extends State<EventCreationScreen> {
                   value!.isEmpty ? 'Please enter a location' : null,
               onSaved: (value) => _location = value!,
             ),
-            // Text input field for the event description
             TextFormField(
               decoration: const InputDecoration(
                 labelText: 'Description',
@@ -155,7 +155,6 @@ class _EventCreationScreenState extends State<EventCreationScreen> {
                   value!.isEmpty ? 'Please enter a description' : null,
               onSaved: (value) => _description = value!,
             ),
-            // Date selection tile
             ListTile(
               leading:
                   const Icon(Icons.calendar_today, color: Pallete.neutral70),
@@ -167,26 +166,24 @@ class _EventCreationScreenState extends State<EventCreationScreen> {
               ),
               onTap: _selectDate,
             ),
-            // Time selection tile
             ListTile(
               leading: const Icon(Icons.access_time, color: Pallete.neutral70),
               title: const Text('Time',
                   style: TextStyle(color: Pallete.neutral70)),
               subtitle: Text(
-                '${_time.format(context)}',
+                _time.format(context),
                 style: const TextStyle(color: Pallete.neutral100),
               ),
               onTap: _selectTime,
             ),
             const SizedBox(height: 16.0),
-            // Button to submit the form and create the event
             ElevatedButton(
               onPressed: _submit,
-              child: Text('Create Event'),
               style: ElevatedButton.styleFrom(
-                primary: Pallete.primary200,
-                onPrimary: Pallete.neutral0,
+                backgroundColor: Pallete.primary200,
+                foregroundColor: Pallete.neutral0,
               ),
+              child: const Text('Create Event'),
             ),
           ],
         ),
