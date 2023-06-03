55dfb6e40d32ded757a2c1826ece4c4a8466cf3b
Kyle Brent Gallego <kylebrentgallegocpe@gmail.com>
2023-06-03 19:02:30 +0800

    update event_model.dart to include list of attendees' userIDs

--- lib/features/events/models/event_model.dart
+++ lib/features/events/models/event_model.dart
@@ -1,11 +1,13 @@
 import 'package:chip_in/constants/appwrite_constants.dart';
 
 class Event {
-  String title;
-  DateTime dateTime;
-  String location;
-  String description;
-  String creatorId;
+  String title; // Event title
+  DateTime dateTime; // Event date and time
+  String location; // Event location
+  String description; // Event description
+  String creatorId; // Event creator's ID
+  List<String>
+      attendeeIds; // Will be used to fetch userIds when users would like to join an event
 
   Event({
     required this.title,
@@ -13,17 +15,24 @@ class Event {
     required this.location,
     required this.description,
     required this.creatorId,
+    this.attendeeIds = const [], // set the default value as an empty list
   });
 
-  // add .fromJson to retrieve documents from Appwrite
-  Event.fromJson(Map<String, dynamic> json)
+/* These are two methods of the Event class in the event_model.dart file. */
+
+  Event.fromJson(
+      Map<String, dynamic>
+          json) // The first method is fromJson which takes a Map<String, dynamic> as input and returns an Event object. This method is used to convert a JSON object to an Event object. It sets the values of the Event object's properties based on the values in the JSON object.
       : title = json['title'],
         dateTime = DateTime.parse(json['date']),
         location = json['location'],
         description = json['description'],
-        creatorId = json['creatorId'];
+        creatorId = json['creatorId'],
+        attendeeIds = List<String>.from(
+            json['attendeeIds'] ?? []); // retrieve the list of attendees
 
   Map<String, dynamic> toJson() {
+    // The second method is toJson which returns a Map<String, dynamic>. This method is used to convert an Event object to a JSON object. It creates a Map with the values of the Event object's properties and returns it.
     return {
       'eventId': AppwriteConstants.eventsCollection,
       'title': title,
@@ -31,6 +40,7 @@ class Event {
       'location': location,
       'description': description,
       'creatorId': creatorId,
+      'attendeeIds': attendeeIds, // add the list of attendees to the json
     };
   }
 }
