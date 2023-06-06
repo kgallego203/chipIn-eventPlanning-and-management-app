28d541c1d663ade0bd3f8651234ffba17200e9ac
Kyle Gallego <kylebrentgallegocpe@gmail.com>
2023-06-06 16:56:01 +0800

    create a user profile model

---
+++ lib/features/profile/models/user_profile_model.dart
@@ -0,0 +1,53 @@
+class UserProfileModel {
+  final String userId; // Unique userIdentifier for the user
+  final String name; // User's full name
+  final String email; // User's email address
+  final String username; // User's username
+  final DateTime dateOfBirth; // User's date of birth
+  final String phoneNumber; // User's phone number
+  final String profilePicture; // URL to user's profile picture
+  final String bio; // Bio about the user
+  final String location; // User's location
+
+  UserProfileModel({
+    required this.userId,
+    required this.name,
+    required this.email,
+    required this.username,
+    required this.dateOfBirth,
+    required this.phoneNumber,
+    required this.profilePicture,
+    required this.bio,
+    required this.location,
+  });
+
+  // Create a User from JSON data
+  factory UserProfileModel.fromJson(Map<String, dynamic> json) {
+    return UserProfileModel(
+      userId: json['userId'],
+      name: json['name'],
+      email: json['email'],
+      username: json['username'],
+      dateOfBirth: DateTime.parse(json['dateOfBirth']),
+      phoneNumber: json['phoneNumber'],
+      profilePicture: json['profilePicture'],
+      bio: json['bio'],
+      location: json['location'],
+    );
+  }
+
+  // Convert our User to JSON to make it easier when we store it in the database
+  Map<String, dynamic> toJson() {
+    return {
+      'userId': userId,
+      'name': name,
+      'email': email,
+      'username': username,
+      'dateOfBirth': dateOfBirth.toIso8601String(),
+      'phoneNumber': phoneNumber,
+      'profilePicture': profilePicture,
+      'bio': bio,
+      'location': location,
+    };
+  }
+}
