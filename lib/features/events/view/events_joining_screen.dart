f06511fce07643c23f6b2b00e11280c9ca08d564
Kyle Gallego <kylebrentgallegocpe@gmail.com>
2023-06-06 18:04:25 +0800

    try to remove the join button for event creators (still in progress)

--- lib/features/events/view/events_joining_screen.dart
+++ lib/features/events/view/events_joining_screen.dart
@@ -1,4 +1,5 @@
-import 'package:chip_in/features/events/widgets/event_card.dart'; // Import the EventCard widget
+import 'package:chip_in/features/auth/services/user_service.dart';
+import 'package:chip_in/features/events/widgets/event_card.dart';
 import 'package:flutter/material.dart';
 import 'package:chip_in/features/events/models/event_model.dart';
 import 'package:chip_in/features/events/services/event_service.dart';
@@ -13,21 +14,25 @@ class JoinEventsScreen extends StatefulWidget {
 }
 
 class _JoinEventsScreenState extends State<JoinEventsScreen> {
-  List<MyEventModel> allEvents = []; // Create a list to hold all the events
+  List<MyEventModel> allEvents = [];
+  String currentUserId = '';
 
   @override
   void initState() {
     super.initState();
-    fetchAllEvents(); // Call the fetchAllEvents method when the screen is initialized
+    fetchAllEvents();
+    getCurrentUserId();
+  }
+
+  Future<void> getCurrentUserId() async {
+    currentUserId = await UserService.getCreatorId();
   }
 
   Future<void> fetchAllEvents() async {
     try {
-      List<MyEventModel> events = await widget.eventService
-          .getAllEvents(); // Call the getAllEvents method of the eventService
+      List<MyEventModel> events = await widget.eventService.getAllEvents();
       setState(() {
-        allEvents =
-            events; // Set the allEvents list to the events returned by the getAllEvents method
+        allEvents = events;
       });
     } catch (error) {
       // Handle error
@@ -38,22 +43,22 @@ class _JoinEventsScreenState extends State<JoinEventsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Join Events'), // Set the title of the app bar
+        title: const Text('Join Events'),
       ),
-      body: allEvents.isEmpty // If there are no events to join
+      body: allEvents.isEmpty
           ? Center(
-              child: Text(
-                  'No events to join'), // Show a message indicating that there are no events to join
+              child: Text('No events to join'),
             )
           : ListView.builder(
-              itemCount: allEvents
-                  .length, // Set the number of items in the list view to the length of the allEvents list
+              itemCount: allEvents.length,
               itemBuilder: (context, index) {
-                MyEventModel event =
-                    allEvents[index]; // Get the event at the current index
+                MyEventModel event = allEvents[index];
+                bool showJoinButton = event.creatorId != currentUserId;
                 return EventCard(
-                    event:
-                        event); // Use the EventCard widget to display the event
+                  event: event,
+                  showJoinButton:
+                      showJoinButton, // Pass the showJoinButton parameter to the EventCard widget
+                );
               },
             ),
     );
