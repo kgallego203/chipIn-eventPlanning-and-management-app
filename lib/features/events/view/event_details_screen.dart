971afa32c4deb2aa8e1976db65681d4a3d7f40d1
Kyle Gallego <kylebrentgallegocpe@gmail.com>
2023-06-12 21:16:52 +0800

    improve the appearance of the event details screen

--- lib/features/events/view/event_details_screen.dart
+++ lib/features/events/view/event_details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:chip_in/themes/palette.dart';
 import 'package:flutter/material.dart';
 import 'package:chip_in/features/events/models/event_model.dart';
+import 'package:intl/intl.dart';
 
 class EventDetailsScreen extends StatefulWidget {
   final MyEventModel event;
@@ -14,58 +15,138 @@ class EventDetailsScreen extends StatefulWidget {
 class _EventDetailsScreenState extends State<EventDetailsScreen> {
   @override
   Widget build(BuildContext context) {
+    final dateFormat = DateFormat('EEEE, MMMM d, y');
+    final timeFormat = DateFormat('h:mm a');
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Pallete.primary200,
-        title: Text(
-          widget.event.title,
-          style: const TextStyle(
+        title: const Text(
+          'Event Details',
+          style: TextStyle(
             color: Pallete.neutral0,
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Title: ${widget.event.title}',
-              style: const TextStyle(
-                color: Pallete.neutral120,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Text(
+                      widget.event.title,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Action to perform when the button is pressed
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Pallete.primary100,
+                    ),
+                    child: const Text(
+                      'Join Event',
+                      style: TextStyle(
+                        color: Pallete.neutral0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: 16.0),
-            Text(
-              'Date: ${widget.event.dateTime}',
-              style: const TextStyle(
-                color: Pallete.neutral70,
-                fontSize: 16.0,
+              const SizedBox(height: 16.0),
+              Table(
+                columnWidths: const {
+                  0: FlexColumnWidth(2),
+                  1: FlexColumnWidth(3),
+                },
+                border: TableBorder.symmetric(
+                  inside: BorderSide.none,
+                  outside: BorderSide.none,
+                ),
+                children: [
+                  _buildTableRow(
+                    'Date',
+                    dateFormat.format(widget.event.dateTime),
+                    Icons.calendar_today,
+                  ),
+                  _buildTableRow(
+                    'Time',
+                    timeFormat.format(widget.event.dateTime),
+                    Icons.access_time,
+                  ),
+                  _buildTableRow(
+                    'Location',
+                    widget.event.location,
+                    Icons.location_on,
+                  ),
+                  _buildTableRow(
+                    'Description',
+                    widget.event.description,
+                    Icons.description,
+                  ),
+                  // Add other event details here
+                  // ...
+                ],
               ),
-            ),
-            const SizedBox(height: 8.0),
-            Text(
-              'Location: ${widget.event.location}',
-              style: const TextStyle(
-                color: Pallete.neutral70,
-                fontSize: 16.0,
+              const SizedBox(height: 16.0),
+              Center(
+                child: Image.network(
+                  'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+                ),
               ),
-            ),
-            const SizedBox(height: 8.0),
-            Text(
-              'Description: ${widget.event.description}',
-              style: const TextStyle(
-                color: Pallete.neutral70,
-                fontSize: 16.0,
-              ),
-            ),
-            // Add other event details here
-            // ...
-          ],
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  TableRow _buildTableRow(String label, String value, IconData icon) {
+    return TableRow(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Icon(
+                icon,
+                color: Pallete.primary100,
+              ),
+              const SizedBox(width: 8.0),
+              Text(
+                label,
+                style: const TextStyle(
+                  color: Pallete.primary100,
+                  fontSize: 18.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            value,
+            style: const TextStyle(
+              color: Pallete.neutral70,
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
