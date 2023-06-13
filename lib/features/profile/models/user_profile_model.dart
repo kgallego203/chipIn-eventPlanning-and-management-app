a8c2d3c18f9b2d45c7f766e09ec485ca5ac0102f
Kyle Gallego <kylebrentgallegocpe@gmail.com>
2023-06-13 16:24:04 +0800

    make some attributes nullable and edit eventId to eventID (console)

--- lib/features/profile/models/user_profile_model.dart
+++ lib/features/profile/models/user_profile_model.dart
@@ -1,31 +1,28 @@
 class UserProfileModel {
-  final String userId; // Unique userIdentifier for the user
   final String name; // User's full name
   final String email; // User's email address
-  final String username; // User's username
-  final DateTime dateOfBirth; // User's date of birth
-  final String phoneNumber; // User's phone number
-  final String profilePicture; // URL to user's profile picture
-  final String location; // User's location
+  final String userID; // Unique userIdentifier for the user
+  final DateTime? dateOfBirth; // User's date of birth
+  final String? phoneNumber; // User's phone number
+  final String? profilePicture; // URL to user's profile picture
+  final String? location; // User's location
 
   UserProfileModel({
-    required this.userId,
     required this.name,
     required this.email,
-    required this.username,
-    required this.dateOfBirth,
-    required this.phoneNumber,
-    required this.profilePicture,
-    required this.location,
+    required this.userID,
+    this.dateOfBirth,
+    this.phoneNumber,
+    this.profilePicture,
+    this.location,
   });
 
   // Create a User from JSON data
   factory UserProfileModel.fromJson(Map<String, dynamic> json) {
     return UserProfileModel(
-      userId: json['userId'],
       name: json['name'],
       email: json['email'],
-      username: json['username'],
+      userID: json['userId'],
       dateOfBirth: DateTime.parse(json['dateOfBirth']),
       phoneNumber: json['phoneNumber'],
       profilePicture: json['profilePicture'],
@@ -36,11 +33,10 @@ class UserProfileModel {
   // Convert our User to JSON to make it easier when we store it in the database
   Map<String, dynamic> toJson() {
     return {
-      'userId': userId,
       'name': name,
       'email': email,
-      'username': username,
-      'dateOfBirth': dateOfBirth.toIso8601String(),
+      'userId': userID,
+      'dateOfBirth': dateOfBirth?.toIso8601String(),
       'phoneNumber': phoneNumber,
       'profilePicture': profilePicture,
       'location': location,
